512d30140a1c9ca2b4c75049456414133f982b57
returner <returner@daum.net>
2019-07-09 14:46:59 +0900

    webpack environment configure for dev and prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,47 @@
 var webpack = require('webpack');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-module.exports = {
-  mode : "development",
-  entry: ['./app/app.ts'],
-  output: {
-    path: path.resolve(__dirname, './docs/dist'),
-    filename: 'app.js',
-    //publicPath : 'dist'
-  },
-  module: {
-    rules: [
-        {
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            use: [
-              {
-                loader: 'ts-loader',
-              },
-            ],
-        },
-    ]
-  },
-  plugins :[
-    new CopyWebpackPlugin([
-      { from : './index.html', to : '../'},
-    ])
-  ],
-  devtool: 'source-map',
-  devServer : {
-    contentBase : path.resolve(__dirname, './'),
-    publicPath: path.resolve(__dirname, '/dist/')
-  },
-  resolve : {
-    extensions: ['.ts', '.tsx', '.js']
+
+module.exports = (env, options) => {
+  const config = {
+    entry: ['./app/app.ts'],
+    module: {
+      rules: [
+          {
+              test: /\.ts$/,
+              exclude: /node_modules/,
+              use: [
+                {
+                  loader: 'ts-loader',
+                },
+              ],
+          }
+      ]
+    },
+    devtool: 'source-map',
+    devServer : {
+      contentBase : path.resolve(__dirname, './'),
+      publicPath: path.resolve(__dirname, '/dist/')
+    },
+    resolve : {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    output : {
+      filename: 'app.js'
+    }
+  };
+
+  if(options.mode === 'development') {
+    config.output.path = path.resolve(__dirname, './dist');
+  } else {
+    config.output.path = path.resolve(__dirname, './docs/dist');
+
+    config.plugins = [
+      new CopyWebpackPlugin([
+        { from : './index.html', to : '../'}
+      ])
+    ];
   }
+
+  return config;
 };
