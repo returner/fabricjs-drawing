c3f598250d009b867d4305539609385b1d3355c8
returner <returner@daum.net>
2019-07-09 17:02:35 +0900

    initialized layout

--- app/App.ts
+++ app/App.ts
@@ -1,7 +1,20 @@
 import {fabric} from "fabric";
-import * as jQuery from "jquery";
+import * as $ from "jquery";
 
 export default class Main{
-
+    public drawCircle() {
+        console.log("circle");
+    }
 }
+
+let main = new Main();
+
+$(document).ready(()=>{
+    $("#btnCircle").on("click", main.drawCircle);
+    $("#btnRectangle").on("click", main.drawCircle);
+    $("#btnTriangle").on("click", main.drawCircle);
+    $("#btnLine").on("click", main.drawCircle);
+    $("#btnDraw").on("click", main.drawCircle);
+})
+
 
